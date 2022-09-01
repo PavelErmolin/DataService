b4cdc6b01ed31669a781fb5076590ec462a24fb3
Kimrayt <103435959+Kimrayt@users.noreply.github.com>
2022-09-01 18:47:17 +0300

    Fixed message listening

--- src/main/java/com/example/redishamster/RedisHamsterApplication.java
+++ src/main/java/com/example/redishamster/RedisHamsterApplication.java
@@ -1,15 +1,22 @@
 package com.example.redishamster;
 
 import Model.IHamsterDao;
+
+import com.example.redishamster.Kafka.MessageListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RedisHamsterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RedisHamsterApplication.class, args);
+    }
+    @Bean
+    public MessageListener messageListener() {
+        return new MessageListener();
     }
 
 }
